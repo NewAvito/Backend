73f41b178701198ae1d65ba6afeaecaf93892937
OlgaGolovkina <ogolovkina13@gmail.com>
2018-08-13 21:26:13 +0300

    Validation for UserProfileCreateSerializer was provided.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -17,3 +17,17 @@ class UserProfileCreateSerializer(ModelSerializer):
         extra_kwargs = {"password":
                             {"write_only": True}
                         }
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        password = validated_data['password']
+        location = validated_data['location']
+        mobile = validated_data['mobile']
+        user_obj = UserProfile(
+            username=username,
+            location=location,
+            mobile=mobile,
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
